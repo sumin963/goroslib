776523d315fd62aa0c3ee6639fb7844363e32f5c
sumin963 <59023842+sumin963@users.noreply.github.com>
2021-01-06 13:27:23 +0900

    Update MsgAckermannDrive.go

--- pkg/msgs/ackermann_msgs/MsgAckermannDrive.go
+++ pkg/msgs/ackermann_msgs/MsgAckermannDrive.go
@@ -2,7 +2,7 @@ package ackermann_msgs
 
 import "github.com/aler9/goroslib/pkg/msg"
 
-type MsgAckemannDrive struct {
+type AckemannDrive struct {
 	msg.Package `ros:"ackermann_msgs"`
 	steering_angel float32
 	steering_angel_velocitiy float32
