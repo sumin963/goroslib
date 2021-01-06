d81264beba7224509f225dd26a32980a85b72de8
sumin963 <59023842+sumin963@users.noreply.github.com>
2021-01-06 13:50:11 +0900

    Update MsgAckermannDriveStamped.go

--- pkg/msgs/ackermann_msgs/MsgAckermannDriveStamped.go
+++ pkg/msgs/ackermann_msgs/MsgAckermannDriveStamped.go
@@ -3,11 +3,10 @@ package ackermann_msgs //nolint:golint
 import (
 	"github.com/aler9/goroslib/pkg/msg"
 	"github.com/aler9/goroslib/pkg/msgs/std_msgs"
-	"github.com/sumin963/goroslib/pkg/msgs/ackermann_msgs"
 )
 
 type AckemannDriveStamped struct {
 	msg.Package `ros:"ackermann_msgs"`
 	Header      std_msgs.Header
-	AckermannDrive ackermann_msgs.MsgAckemannDrive
+	AckermannDrive AckemannDrive
 }
