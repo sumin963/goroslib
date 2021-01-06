2b026521c49c280fffb3c602d2d549536bbc4252
sumin963 <59023842+sumin963@users.noreply.github.com>
2021-01-06 11:10:30 +0900

    Create MsgAckermannDrive.go

---
+++ pkg/msgs/ackermann_msgs/MsgAckermannDrive.go
@@ -0,0 +1,12 @@
+package ackermann_msgs
+
+import "github.com/aler9/goroslib/pkg/msg"
+
+type MsgAckemannDrive struct {
+	msg.Package 'ros:"nav_msgs"'
+	steering_angel float32
+	steering_angel_velocitiy float32
+	speed float32
+	acceleration float32
+	jerk float32
+}
